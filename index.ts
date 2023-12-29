02b881614862f9a40901359f123ca73e8372d8e5
wisdomstar94 <wisdomstar94@gmail.com>
2023-12-29 15:37:23 +0900

    v0.0.7 : 내보내기 수정.

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export * from './src/hooks/use-multiple-api-manager/use-multiple-api-manager.hook';
+export * from './src/hooks/use-multiple-api-manager/use-multiple-api-manager.interface';
+export * from './src/hooks/use-multiple-sync-api-manager/use-multiple-sync-api-manager.hook';
 export * from './src/hooks/use-multiple-sync-api-manager/use-multiple-sync-api-manager.interface';
